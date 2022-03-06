8f272d0cb1fdc1d5efdc89e3aa17856b2e3865b5
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-06 17:02:29 -0600

    Update Workers-With-The-Highest-Salaries.py

--- Workers-With-The-Highest-Salaries.py
+++ Workers-With-The-Highest-Salaries.py
@@ -8,3 +8,4 @@ title
 df1 = pd.merge(worker, title, how = 'left', left_on = 'worker_id', right_on = 'worker_ref_id').sort_values('salary', ascending = False)
 
 df2 = df1.query('salary == salary.max()')['worker_title']
+df1[df1.salary == df1.salary.max()]['worker_title']
